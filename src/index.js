714cca845d25eb0ccc182cfffaad99263e6e08ba
DESKTOP-VU5U251\wilfrido <wgarcia@eurekain.com>
2019-01-26 18:54:06 -0500

    multiplicacion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Suma = (props) => <h1>Suma 1... {props.count} </h1>
+const Suma2 = (props) => <h1>Suma 2... {props.count} </h1>
+const App = (props) =><div><Suma count = {props.count}/><Suma2 count = {props.count} /></div> 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App count="2" />, document.getElementById('root'));
+
+
+const reducer = (state, action) => {
+    if(action.type === 'PROD'){
+        return state * action.count;
+    }
+    return state;
+};
+const store = createStore(reducer,2);
+
+store.subscribe(()=>{
+console.log('New dispatch', store.getState());
+ReactDOM.render(<App count={store.getState()} />, document.getElementById('root'));
+});
+setInterval(() => {
+    store.dispatch({type:'PROD', count: 2});
+
+}, 1000)
